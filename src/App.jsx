ea5521384a3eea74ff52d9d8969752abb4e24293
Dennis Kestone <denniskestone@gmail.com>
2024-01-02 22:26:05 +0300

    node modules deleted and navbar styled

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./App.css";
+import "./index.css";
+
+import {
+  Header,
+  Footer,
+  WhatGPT3,
+  Features,
+  Blog,
+  Possibility,
+} from "./container";
+
+import { Article, CTA, Feature, Brand, Navbar } from "./components";
+function App() {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/container/index.js
@@ -0,0 +1,6 @@
+export { default as Blog } from "./blog/Blog";
+export { default as Header } from "./header/Header";
+export { default as Footer } from "./footer/Footer";
+export { default as Features } from "./Features/Features";
+export { default as Possibility } from "./Possibility/Possibility";
+export { default as WhatGPT3 } from "./WhatGPT3/WhatGPT3";
