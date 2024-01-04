ecc620288c2d6ab84c4355553382b2167a48ab02
Dennis Kestone <denniskestone@gmail.com>
2024-01-04 14:24:12 +0300

    input button

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,35 +1,35 @@
 import React from "react";
-import "./header.css";
-import image from "../../assets/people.png";
+import people from "../../assets/people.png";
 import ai from "../../assets/ai.png";
+import "./header.css";
 
-function Header() {
-  return (
-    <div className="gpt3__header section-padding" id="home">
-      <div className="gpt3__header-content">
-        <h1 className="gradient__text">
-          Let's build something amazing with GPT-3 open AI
-        </h1>
-        <p>
-          Yet bed for travelling assistance indulgence unpleasing. Not thoughts
-          all exercise blessing.Indulgence way everything joy alteration
-          boisterous the attachment. Party we years to order allow asked for.
-        </p>
-        <div className="gpt3__header-content__input">
-          <input type="email" placeholder="Your email address" />
-          <button type="button">Get started</button>
-        </div>
-        <div className="gpt3__header-content__people">
-          <img src={image} alt="people image" />
-          <p>1600 requested access in the last 24hrs</p>
-        </div>
+const Header = () => (
+  <div className="gpt3__header section__padding" id="home">
+    <div className="gpt3__header-content">
+      <h1 className="gradient__text">
+        Let&apos;s Build Something amazing with GPT-3 OpenAI
+      </h1>
+      <p>
+        Yet bed any for travelling assistance indulgence unpleasing. Not
+        thoughts all exercise blessing. Indulgence way everything joy alteration
+        boisterous the attachment. Party we years to order allow asked of.
+      </p>
 
-        <div className="gpt3__header-image">
-          <img src={ai} alt="ai" />
-        </div>
+      <div className="gpt3__header-content__input">
+        <input type="email" placeholder="Your Email Address" />
+        <button type="button">Get Started</button>
+      </div>
+
+      <div className="gpt3__header-content__people">
+        <img src={people} />
+        <p>1,600 people requested access a visit in last 24 hours</p>
       </div>
     </div>
-  );
-}
+
+    <div className="gpt3__header-image">
+      <img src={ai} />
+    </div>
+  </div>
+);
 
 export default Header;
