2f0155e3d7b164372ed5419caf2461003f3624ec
Dennis Kestone <denniskestone@gmail.com>
2024-01-05 22:05:50 +0300

    Update whatGPT3 CSS

--- src/container/WhatGPT3/WhatGPT3.jsx
+++ src/container/WhatGPT3/WhatGPT3.jsx
@@ -1,46 +1,23 @@
 import React from "react";
 import "./whatGPT3.css";
+import { Feature } from "../../components";
 
 function WhatGPT3() {
   return (
-    <div className="gpt3__content">
-      <div className="gpt3__top-content">
-        <div className="gpt3__left">What is GPT-3</div>
-        <div className="gpt3__right">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis
-          doloremque asperiores ex rerum hic magni quo impedit dicta, dolorum
-          ducimus laboriosam voluptatum error aliquam, sint odio necessitatibus
-          quae nihil dolore!
-        </div>
+    <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature />
       </div>
-      <div className="gpt3__bottom-content">
-        <div className="gpt3__1">
-          <h1>Chatbots</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
-            illum eum repellendus distinctio inventore recusandae commodi sequi
-            ipsam similique expedita quam soluta voluptates obcaecati aliquam
-            molestiae, alias velit exercitationem autem?
-          </p>
-        </div>
-        <div className="gpt3__2">
-          <h1>Chatbots</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
-            illum eum repellendus distinctio inventore recusandae commodi sequi
-            ipsam similique expedita quam soluta voluptates obcaecati aliquam
-            molestiae, alias velit exercitationem autem?
-          </p>
-        </div>
-        <div className="gpt3__3">
-          <h1>Chatbots</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
-            illum eum repellendus distinctio inventore recusandae commodi sequi
-            ipsam similique expedita quam soluta voluptates obcaecati aliquam
-            molestiae, alias velit exercitationem autem?
-          </p>
-        </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">
+          The possibilities are beyond your imagination.
+        </h1>
+        <p>Explore the library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Feature />
+        <Feature />
+        <Feature />
       </div>
     </div>
   );
