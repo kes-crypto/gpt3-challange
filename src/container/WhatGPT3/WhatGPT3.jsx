9f017f3f0012f1574ab658ad938dd43fe11ea166
Dennis Kestone <denniskestone@gmail.com>
2024-01-05 21:16:45 +0300

    Update on whatisChatGPT3

--- src/container/WhatGPT3/WhatGPT3.jsx
+++ src/container/WhatGPT3/WhatGPT3.jsx
@@ -3,8 +3,45 @@ import "./whatGPT3.css";
 
 function WhatGPT3() {
   return (
-    <div>
-      <h1>WhatGPT3</h1>
+    <div className="gpt3__content">
+      <div className="gpt3__top-content">
+        <div className="gpt3__left">What is GPT-3</div>
+        <div className="gpt3__right">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis
+          doloremque asperiores ex rerum hic magni quo impedit dicta, dolorum
+          ducimus laboriosam voluptatum error aliquam, sint odio necessitatibus
+          quae nihil dolore!
+        </div>
+      </div>
+      <div className="gpt3__bottom-content">
+        <div className="gpt3__1">
+          <h1>Chatbots</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+            illum eum repellendus distinctio inventore recusandae commodi sequi
+            ipsam similique expedita quam soluta voluptates obcaecati aliquam
+            molestiae, alias velit exercitationem autem?
+          </p>
+        </div>
+        <div className="gpt3__2">
+          <h1>Chatbots</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+            illum eum repellendus distinctio inventore recusandae commodi sequi
+            ipsam similique expedita quam soluta voluptates obcaecati aliquam
+            molestiae, alias velit exercitationem autem?
+          </p>
+        </div>
+        <div className="gpt3__3">
+          <h1>Chatbots</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+            illum eum repellendus distinctio inventore recusandae commodi sequi
+            ipsam similique expedita quam soluta voluptates obcaecati aliquam
+            molestiae, alias velit exercitationem autem?
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
