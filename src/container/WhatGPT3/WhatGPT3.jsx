ca882f46fd916cac0168d351d41336980d12b62a
Dennis Kestone <denniskestone@gmail.com>
2024-01-10 23:11:44 +0300

    Features component

--- src/container/WhatGPT3/WhatGPT3.jsx
+++ src/container/WhatGPT3/WhatGPT3.jsx
@@ -7,7 +7,7 @@ const WhatGPT3 = () => (
     <div className="gpt3__whatgpt3-feature">
       <Feature
         title="What is GPT-3"
-        text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."
+        text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut id mollitia odio, debitis aliquid dolorem ratione ducimus vitae tenetur voluptatum in magni tempora pariatur natus modi eius et corporis quasi."
       />
     </div>
     <div className="gpt3__whatgpt3-heading">
