a015f448ac01368f53b9694021ab67bb38a6d124
Dennis Kestone <denniskestone@gmail.com>
2024-01-11 20:51:37 +0300

    Update index.js

--- src/container/index.js
+++ src/container/index.js
@@ -1,4 +1,4 @@
-export { default as Blog } from "./blog/Blog";
+
 export { default as Header } from "./header/Header";
 export { default as Footer } from "./footer/Footer";
 export { default as Features } from "./Features/Features";
